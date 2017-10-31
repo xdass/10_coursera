27cce23ef724cbdbd5489c099c35dba20ee6da46
xdass <420600@gmail.com>
2017-10-31 22:44:20 +0500

    Add parse xml from site

--- coursera.py
+++ coursera.py
@@ -1,7 +1,13 @@
+import requests
+import bs4
+import openpyxl
+from lxml import etree
 
 
 def get_courses_list():
-    pass
+    result = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
+    tree = etree.fromstring(result.content)
+    # courses_links = [item[0].text for item in tree]
 
 
 def get_course_info(course_slug):
@@ -13,4 +19,4 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    print(get_courses_list())
