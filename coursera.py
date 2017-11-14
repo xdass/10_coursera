28a22765808976d1ebd383609e694cbe853e840f
xdass <420600@gmail.com>
2017-11-14 22:41:45 +0500

    Split functions into small different to divide actions

--- coursera.py
+++ coursera.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import bs4
 from openpyxl import Workbook
@@ -13,9 +14,13 @@ def get_courses_url_list(courses_to_parse=20):
     return links[:courses_to_parse]
 
 
-def get_course_info(course_url):
-    html_response = requests.get(course_url, headers=HEADER_FOR_RU)
-    soup = bs4.BeautifulSoup(html_response.content, 'lxml')
+def get_course_page(url):
+    html_response = requests.get(url, headers=HEADER_FOR_RU)
+    return html_response.content
+
+
+def parse_course_info(course_html):
+    soup = bs4.BeautifulSoup(course_html, 'lxml')
     course_title = soup.select_one('.title').string
     course_start_date = soup.select_one('.startdate span').string
     course_language = soup.select_one('.rc-Language').text
@@ -23,7 +28,7 @@ def get_course_info(course_url):
     if course_rating:
         course_rating = course_rating.string
     else:
-        course_rating = 'Нет данных'
+        course_rating = None
     course_weeks = len(soup.select('.rc-WeekView > div'))
     return {
         'course_title': course_title,
@@ -37,11 +42,12 @@ def get_course_info(course_url):
 def collect_courses_info(url_list):
     courses_list = []
     for url in url_list:
-        courses_list.append(get_course_info(url))
+        course_page = get_course_page(url)
+        courses_list.append(parse_course_info(course_page))
     return courses_list
 
 
-def output_courses_info_to_xlsx(courses):
+def fill_excel_workbook(courses):
     wb = Workbook()
     ws = wb.active
     ws.title = 'Coursera courses'
@@ -60,7 +66,12 @@ def output_courses_info_to_xlsx(courses):
         ws['C{}'.format(row)] = course_info['course_start_date']
         ws['D{}'.format(row)] = course_info['course_weeks']
         ws['E{}'.format(row)] = course_info['course_rating']
-    wb.save('coursera_courses.xlsx')
+    return wb
+
+
+def save_to_xlsx(workbook, filepath='coursera_courses.xlsx'):
+    workbook.save(filepath)
+
 
 if __name__ == '__main__':
     print('Parse courses links....')
@@ -68,4 +79,9 @@ if __name__ == '__main__':
     print('Start parsing courses data....')
     courses_info_list = collect_courses_info(courses_links)
     print('Save to file')
-    output_courses_info_to_xlsx(courses_info_list)
+    filled_workbook = fill_excel_workbook(courses_info_list)
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        save_to_xlsx(filled_workbook, filepath)
+    else:
+        save_to_xlsx(filled_workbook)
